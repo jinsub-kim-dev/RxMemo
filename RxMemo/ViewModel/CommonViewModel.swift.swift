0a814c8ea0d71b5446c475f8d73f585aeb450767
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2022-07-10 19:42:07 +0900

    Create Common ViewModel

---
+++ RxMemo/ViewModel/CommonViewModel.swift.swift
@@ -0,0 +1,23 @@
+//
+//  CommonViewModel.swift.swift
+//  RxMemo
+//
+//  Created by Jinsub on 2022/07/10.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CommonViewModel: NSObject {
+    let title: Driver<String>
+    
+    let sceneCoordinator: SceneCoordinatorType
+    let storage: MemoStorageType
+    
+    init(title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.title = Observable.just("title").asDriver(onErrorJustReturn: "")
+        self.sceneCoordinator = sceneCoordinator
+        self.storage = storage
+    }
+}
